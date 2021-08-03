d4ee1a30e71e521e41215fbbfccc47273fec0f20
Yuumataiki <84608371+Yuumataiki@users.noreply.github.com>
2021-08-03 15:47:34 +0900

    Initial Commit

---
+++ SwiftUI-Chapter2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Chapter2
+//
+//  Created by yumataiki on 2021/08/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Chapter2/SwiftUI_Chapter2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Chapter2App.swift
+//  SwiftUI-Chapter2
+//
+//  Created by yumataiki on 2021/08/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Chapter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
